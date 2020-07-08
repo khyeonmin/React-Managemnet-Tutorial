07e7800ea04da9f5b6ac5dd160e00c54e42374b8
KANGHYEONMIN <kanghyeonmin@gmail.com>
2020-07-09 00:55:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src = {logo} lat="logo"/>
-      <h2>hyeonmin develop management system</h2>
-    </div>
-  );
+const customers = [{
+  'id' : 'hmin',
+  'image' : 'https://placeimg.com/64/64/1',
+  'name': 'Kang Hyeon Min',
+  'birthday': '1994.08.08',
+  'gender': 'Male',
+  'job': 'M.S Candidate',
+},
+{
+  'id' : 'ez1',
+  'image' : 'https://placeimg.com/64/64/2',
+  'name': 'Lee Jee Won',
+  'birthday': '1994.12.13',
+  'gender': 'Female',
+  'job': 'Developer'
+},
+{
+  'id' : 'ChaeWon',
+  'image' : 'https://placeimg.com/64/64/3',
+  'name': 'Lee Chae Won',
+  'birthday': '1995.02.05',
+  'gender': 'Female',
+  'job': 'CEO'
+}
+];
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        { customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}/>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
